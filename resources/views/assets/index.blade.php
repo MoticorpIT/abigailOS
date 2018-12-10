a8919b4acb0b79ce9645028e732562fd33b41da1
iAmBentley <joey@moticorp.com>
2018-12-10 16:20:28 -0500

    Add tables to account, asset, company, tenant indexes

--- resources/views/assets/index.blade.php
+++ resources/views/assets/index.blade.php
@@ -3,11 +3,30 @@
 @section('content')
 	Assets - index.blade.php
 	<hr>
-	@foreach($assets as $asset)
-		{{ $asset->name }}
-		<a href="assets/{{ $asset->id }}/edit">Edit asset</a>
-		<a href="assets/{{ $asset->id }}">Show asset</a><br />
-	@endforeach
 	<a href="assets/create">Create asset</a>
+	@foreach($assets as $asset)
+		<ul>
+			<li>id: {{ $asset->id }}</li>
+			<li>name: {{ $asset->name }}</li>
+			<li>street_1: {{ $asset->street_1 }}</li>
+			<li>street_2: {{ $asset->street_2 }}</li>
+			<li>city: {{ $asset->city }}</li>
+			<li>state: {{ $asset->state }}</li>
+			<li>zip: {{ $asset->zip }}</li>
+			<li>phone_1: {{ $asset->phone_1 }}</li>
+			<li>phone_2: {{ $asset->phone_2 }}</li>
+			<li>fax: {{ $asset->fax }}</li>
+			<li>email: {{ $asset->email }}</li>
+			<li>rent: {{ $asset->rent }}</li>
+			<li>deposit: {{ $asset->deposit }}</li>
+			<li>asset_type_id: {{ $asset->asset_type_id }}</li>
+			<li>company_id: {{ $asset->company_id }}</li>
+			<li>status_id: {{ $asset->status_id }}</li>
+			<li>created_at: {{ $asset->created_at }}</li>
+			<li>updated_at: {{ $asset->updated_at }}</li>
+			<li><a href="assets/{{ $asset->id }}/edit">Edit asset</a></li>
+			<li><a href="assets/{{ $asset->id }}">Show asset</a></li>
+		</ul>
+	@endforeach
 @endsection
 
--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,11 +3,34 @@
 @section('content')
 	Companies - index.blade.php
 	<hr>
-	@foreach($companies as $company)
-		{{ $company->name }}
-		<a href="companies/{{ $company->id }}/edit">Edit company</a>
-		<a href="companies/{{ $company->id }}">Show company</a><br />
-	@endforeach
 	<a href="companies/create">Create company</a>
+	@foreach($companies as $company)
+		<ul>
+			<li>id: {{ $company->id }}</li>
+			<li>name: {{ $company->name }}</li>
+			<li>street_1: {{ $company->street_1 }}</li>
+			<li>street_2: {{ $company->street_2 }}</li>
+			<li>city: {{ $company->city }}</li>
+			<li>state: {{ $company->state }}</li>
+			<li>zip: {{ $company->zip }}</li>
+			<li>phone_1: {{ $company->phone_1 }}</li>
+			<li>phone_2: {{ $company->phone_2 }}</li>
+			<li>fax: {{ $company->fax }}</li>
+			<li>email: {{ $company->email }}</li>
+			<li>logo: {{ $company->logo }}</li>
+			<li>incorp_date: {{ $company->incorp_date }}</li>
+			<li>corp_id: {{ $company->corp_id }}</li>
+			<li>city_lic: {{ $company->city_lic }}</li>
+			<li>county_lic: {{ $company->county_lic }}</li>
+			<li>fed_tax_id: {{ $company->fed_tax_id }}</li>
+			<li>company_type_id: {{ $company->company_type_id }}</li>
+			<li>status_id: {{ $company->status_id }}</li>
+			<li>created_at: {{ $company->created_at }}</li>
+			<li>updated_at: {{ $company->updated_at }}</li>
+			<li><a href="companies/{{ $company->id }}/edit">Edit company</a></li>
+			<li><a href="companies/{{ $company->id }}">Show company</a></li>
+		</ul>
+	@endforeach
+
 @endsection
 
