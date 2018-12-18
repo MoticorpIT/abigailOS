8eb96c5612e12d0017892aabc8bbb80dfed588eb
iAmBentley <joey@moticorp.com>
2018-12-18 11:40:27 -0500

    Add company.create functionality

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Company;
+use App\CompanyType;
 use App\Account;
 use App\Asset;
 use App\Note;
@@ -29,7 +30,9 @@ class CompanyController extends Controller
      */
     public function create()
     {
-        return view('companies.create');
+        $company_types = CompanyType::all();
+        $statuses = Status::where('is_active', 1)->get();
+        return view('companies.create', compact('company_types', 'statuses'));
     }
 
     /**
@@ -40,7 +43,62 @@ class CompanyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         /* VALIDATE DATA COMING IN FROM FORM */
+        $this->validate(request(), [
+            'name' => 'unique:companies|required',
+            'street_1' => 'required', 
+            'street_2' => 'nullable',
+            'city' => 'required', 
+            'state' => 'required', 
+            'zip' => 'required', 
+            'phone_1' => 'required', 
+            'phone_2' => 'nullable',
+            'fax' => 'nullable',
+            'email' => 'nullable',
+            'logo' => 'nullable',
+            'incorp_date' => 'nullable',
+            'corp_id' => 'nullable',
+            'city_lic' => 'nullable',
+            'county_lic' => 'nullable',
+            'fed_tax_id' => 'nullable',
+            'company_type_id' => 'required', 
+            'status_id' => 'required',
+        ]);
+        /* CREATE NEW COMPANY */
+        $company = new Company(
+            [
+                'name' => $request->name,
+                'street_1' => $request->street_1,
+                'street_2' => $request->street_2,
+                'city' => $request->city,
+                'state' => $request->state,
+                'zip' => $request->zip,
+                'phone_1' => $request->phone_1,
+                'phone_2' => $request->phone_2,
+                'fax' => $request->fax,
+                'email' => $request->email,
+                'logo' => $request->logo,
+                'incorp_date' => $request->incorp_date,
+                'corp_id' => $request->corp_id,
+                'city_lic' => $request->city_lic,
+                'county_lic' => $request->county_lic,
+                'fed_tax_id' => $request->fed_tax_id,
+                'company_type_id' => $request->company_type_id,
+                'status_id' => $request->status_id,
+            ]
+        );
+        /* SAVE NEW COMPANY TO DATABASE */
+        $company->save();
+        // $company->categories()->attach($request->category);
+        // $company->sizes()->attach($request->size);
+        // $company->stores()->attach($request->store);
+        /* CONFIRM CREATION AND REDIRECT USER */
+        // if (!$company->save()) {
+        //     session()->flash('message', 'Contact Manager. ERROR: Company did not save');
+        // } else {
+        //     session()->flash('message', 'Company Created Successfully');
+        // }
+        return redirect('/companies');
     }
 
     /**
--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -1,6 +1,134 @@
 @extends('layouts.app')
 
 @section('content')
-Companies - create.blade.php
+<h1>Add New Company</h1>
+<form method="POST" action="/companies">
+	{{ csrf_field() }}
+	<div class="row mb-5">
+		<div class="col-md-6">
+			{{-- COMPANY NAME --}}
+			<div class="form-group">
+				<label>Name</label>
+				<input type="text" class="form-control {{ $errors->has('name') ? 'has-error' : '' }}" name="name" placeholder="Name" value="{{ old('name') }}" autofocus>
+			</div>
+			{{-- COMPANY STREET_1 --}}
+			<div class="form-group">
+				<label>Street</label>
+				<input type="text" class="form-control {{ $errors->has('street_1') ? 'has-error' : '' }}" name="street_1" placeholder="Street" value="{{ old('street_1') }}">
+			</div>
+			{{-- COMPANY STREET_2 --}}
+			<div class="form-group">
+				<label>Street (optional)</label>
+				<input type="text" class="form-control {{ $errors->has('street_2') ? 'has-error' : '' }}" name="street_2" placeholder="Street (optional)" value="{{ old('street_2') }}">
+			</div>
+			{{-- COMPANY CITY --}}
+			<div class="form-group">
+				<label>City</label>
+				<input type="text" class="form-control {{ $errors->has('city') ? 'has-error' : '' }}" name="city" placeholder="City" value="{{ old('city') }}">
+			</div>
+			{{-- COMPANY STATE --}}
+			<div class="form-group">
+				<label>State</label>
+				<input type="text" class="form-control {{ $errors->has('state') ? 'has-error' : '' }}" name="state" placeholder="State" value="{{ old('state') }}">
+			</div>
+			{{-- COMPANY ZIP --}}
+			<div class="form-group">
+				<label>Zip</label>
+				<input type="text" class="form-control {{ $errors->has('zip') ? 'has-error' : '' }}" name="zip" placeholder="Zip" value="{{ old('zip') }}">
+			</div>
+		</div>
+
+		<div class="col-md-6">
+			{{-- COMPANY PHONE_1 --}}
+			<div class="form-group">
+				<label>Phone</label>
+				<input type="text" class="form-control {{ $errors->has('phone_1') ? 'has-error' : '' }}" name="phone_1" placeholder="Phone" value="{{ old('phone_1') }}">
+			</div>
+			{{-- COMPANY PHONE_2 --}}
+			<div class="form-group">
+				<label>Phone (optional)</label>
+				<input type="text" class="form-control {{ $errors->has('phone_2') ? 'has-error' : '' }}" name="phone_2" placeholder="Phone (optional)" value="{{ old('phone_2') }}">
+			</div>
+			{{-- COMPANY FAX --}}
+			<div class="form-group">
+				<label>Fax</label>
+				<input type="text" class="form-control {{ $errors->has('fax') ? 'has-error' : '' }}" name="fax" placeholder="Fax" value="{{ old('fax') }}">
+			</div>
+			{{-- COMPANY EMAIL --}}
+			<div class="form-group">
+				<label>Email</label>
+				<input type="text" class="form-control {{ $errors->has('email') ? 'has-error' : '' }}" name="email" placeholder="Email" value="{{ old('email') }}">
+			</div>
+			{{-- COMPANY LOGO --}}
+			<div class="form-group">
+				<label>Logo - This should be an uploader</label>
+				<input type="text" class="form-control {{ $errors->has('logo') ? 'has-error' : '' }}" name="logo" placeholder="Path to the Company Logo" value="{{ old('logo') }}">
+			</div>
+		</div>
+
+	</div>
+	<div class="row mb-5">
+
+		<div class="col-md-6">
+			{{-- COMPANY INCORP_DATE --}}
+			<div class="form-group">
+				<label>Incorporated Date</label>
+				<input type="text" class="form-control {{ $errors->has('incorp_date') ? 'has-error' : '' }}" name="incorp_date" placeholder="Date Company was Incorporated" value="{{ old('incorp_date') }}">
+			</div>
+			{{-- COMPANY CORP_ID --}}
+			<div class="form-group">
+				<label>Corporation ID</label>
+				<input type="text" class="form-control {{ $errors->has('corp_id') ? 'has-error' : '' }}" name="corp_id" placeholder="Corporation Id" value="{{ old('corp_id') }}">
+			</div>
+			{{-- COMPANY CITY_LIC --}}
+			<div class="form-group">
+				<label>City License</label>
+				<input type="text" class="form-control {{ $errors->has('city_lic') ? 'has-error' : '' }}" name="city_lic" placeholder="City License" value="{{ old('city_lic') }}">
+			</div>
+			{{-- COMPANY COUNTY_LIC --}}
+			<div class="form-group">
+				<label>County License</label>
+				<input type="text" class="form-control {{ $errors->has('county_lic') ? 'has-error' : '' }}" name="county_lic" placeholder="County License" value="{{ old('county_lic') }}">
+			</div>
+			{{-- COMPANY FED_TAX_ID --}}
+			<div class="form-group">
+				<label>Federal Tax Id</label>
+				<input type="text" class="form-control {{ $errors->has('fed_tax_id') ? 'has-error' : '' }}" name="fed_tax_id" placeholder="Federal Tax Id" value="{{ old('fed_tax_id') }}">
+			</div>
+		</div>
+
+		
+		<div class="col-md-6">
+			{{-- COMPANY TYPE DROPDOWN --}}
+			<div class="form-group">
+				<label for="company_type_id">Company Type</label>
+				<select class="form-control" id="company_type_id" name="company_type_id">
+					<option value="">Choose One</option>
+					@foreach($company_types as $company_type)
+						<option value="{{ $company_type->id }}">{{ $company_type->name }}</option>
+					@endforeach
+				</select>
+			</div>
+			{{-- STATUS DROPDOWN --}}
+			<div class="form-group">
+				<label for="status_id">Company Type</label>
+				<select class="form-control" id="status_id" name="status_id">
+					@foreach($statuses as $status)
+						<option value="{{ $status->id }}">{{ $status->name }}</option>
+					@endforeach
+				</select>
+			</div>
+		</div>
+
+	</div>
+	{{-- BUTTON SET --}}
+	<div class="row mb-5">
+		<div class="col-md-12">
+			<a href="{{ route('companies.index') }}" class="btn btn-round">Cancel</a>
+			<button id="submit-btn" type="submit" class="btn btn-danger btn-round">Save Company</button>
+		</div>
+	</div>
+</form>
+
 @endsection
 
