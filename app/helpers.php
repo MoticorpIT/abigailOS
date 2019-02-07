1bc68e49d0dea6f2b063307f8d5a1b09eaeb8835
iAmBentley <joey@moticorp.com>
2019-02-07 12:13:40 -0500

    Add date + money formatting helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+// FORMAT PHONE NUMBERS
 if (! function_exists('cleanPhone')) {
     function cleanPhone($phoneNumber) {
 		
@@ -11,9 +12,49 @@ if (! function_exists('cleanPhone')) {
 
 		return $phoneNumber;
 
+		// NO EXTENSION ----->		'~.*(\d{3})[^\d]*(\d{3})[^\d]*(\d{4})(?:[ \D#\-]*(\d{3,6}))?.*~'
+		// WITH EXTENSION ----->	'~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4})[^\d]{0,7}](\d{0,5}).*~'
+
     }
 }
 
-// NO EXTENSION ----->		'~.*(\d{3})[^\d]*(\d{3})[^\d]*(\d{4})(?:[ \D#\-]*(\d{3,6}))?.*~'
-// WITH EXTENSION ----->	'~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4})[^\d]{0,7}](\d{0,5}).*~'
+// FORMAT DOLLAR AMOUNTS
+if (! function_exists('cleanMoney')) {
+    function cleanMoney($amount) {
+
+		$amount = '$' . number_format($amount);
+
+		return $amount;
+
+    }
+}
+
+// FORMAT DOLLAR AMOUNTS WITH CENTS
+if (! function_exists('cleanMoneyWithCents')) {
+    function cleanMoneyWithCents($amount) {
+
+		$amount = '$' . number_format($amount, 2);
+
+		return $amount;
+
+    }
+}
+
+// FORMAT DATES
+if (! function_exists('cleanDate')) {
+    function cleanDate($date) {
+
+		return $date->format('m/d/y');
+
+    }
+}
+
+// FORMAT DATES w TIME
+if (! function_exists('cleanDateWithTime')) {
+    function cleanDateWithTime($date) {
+
+    	return $date->format('m/d/y h:i a');
+
+    }
+}
 
